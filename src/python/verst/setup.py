3e5bc4585bda0a4c421e812e6cdd68fdab7f9462
MasterDDT <miteshp@live.com>
2017-11-22 13:18:08 -0600

    Add setup

---
+++ src/python/verst/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='verst',
+      version='0.1',
+      description='Verst pants plugins',
+      url='http://actioniq.com',
+      author='Mitesh Patel',
+      author_email='mitesh@actioniq.com',
+      license='MIT',
+      packages=['verst'],
+      zip_safe=False)
